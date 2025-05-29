de4a9fe8ef998dab095a34e0e330dd608fb9958e
kruall <kru.bash.all@gmail.com>
2025-05-30 01:02:28 +0300

    [TM_WEB-9] Replace inline styles with CSS modules (#39)

--- react-dashboard/pages/kanban.js
+++ react-dashboard/pages/kanban.js
@@ -1,12 +1,13 @@
 import Kanban from '../components/Kanban'
 import Navigation from '../components/Navigation'
 import useTasks from '../hooks/useTasks'
+import styles from './Page.module.css'
 
 export default function KanbanPage() {
   const tasks = useTasks()
 
   return (
-    <div style={{ padding: 16 }}>
+    <div className={styles.container}>
       <Navigation />
       <h1>Kanban Board</h1>
       <Kanban tasks={tasks} />
