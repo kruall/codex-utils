9f66731e6b4931aa4c4d75bbd5e6ea0a693922e1
kruall <kru.bash.all@gmail.com>
2025-05-30 17:15:00 +0300

    [DEV-22] Separate React tests job

---
+++ react-dashboard/components/Navigation.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from '@testing-library/react'
+import Navigation from './Navigation'
+
+test('renders navigation links', () => {
+  render(<Navigation />)
+  expect(screen.getByText(/Home/)).toBeInTheDocument()
+  expect(screen.getByText(/Task List/)).toBeInTheDocument()
+  expect(screen.getByText(/Kanban Board/)).toBeInTheDocument()
+  expect(screen.getByText(/TODO Tasks/)).toBeInTheDocument()
+})
